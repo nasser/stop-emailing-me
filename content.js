7cb90ade8ea11467effd2f94325c7992e781b862
Ramsey Nasser <ram@nas.sr>
2018-10-23 14:16:05 -0400

    Also click on Unsubscribe links

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if( request.message === "clicked_browser_action" ) {
-			Array.prototype.slice.call(document.querySelectorAll("a")).filter(a => a.innerText.includes("unsubscribe")).forEach(a => a.click());
+			Array.prototype.slice.call(document.querySelectorAll("a")).filter(a => a.innerText.includes("unsubscribe") ||
+				                                                               a.innerText.includes("Unsubscribe")).forEach(a => a.click());
 		}
 	}
 );
